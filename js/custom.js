3d04fadfdb098679d3f15c7c5bc158a5fa1b09cb
Bash2009 <athekamebash@gmail.com>
2025-07-29 13:31:57 +0100

    Fixed bug with signup slider

--- js/custom.js
+++ js/custom.js
@@ -5,15 +5,12 @@ const email = $("#email-field");
 const password = $("#password-field");
 const cpassword = $("#cpassword-field");
 
-const country = $("#country-field");
-const address = $("#address-field");
-const education = $("#education-field");
-const skills = $("#skills-field");
-
 const phone = $("#phone-field");
 const gender = $("#gender-field");
 const age = $("#age-field");
 const bio = $("#bio-field");
+const sign_submit = document.getElementById("signup_submit");
+let current_slide = 1;
 
 // to get current year
 function getYear() {
@@ -58,9 +55,9 @@ $(".client_owl-carousel").owlCarousel({
 $(".next-fields").click((ev) => {
   let slide = ev.target.closest(".field-section");
   let nextSlide = slide.nextElementSibling;
-  let prev = ev.target.previousElementSibling;
   let alert = $("#signup-warning");
-  if (!prev) {
+
+  if (current_slide == 1) {
     if (
       fname.val() == "" ||
       lname.val() == "" ||
@@ -87,10 +84,31 @@ $(".next-fields").click((ev) => {
       slide.classList.add("d-none");
       nextSlide.classList.remove("d-none");
       nextSlide.classList.add("d-block");
-      alert.fadeOut().removeClass("show");
+      current_slide = 2;
+      console.log("not");
     }
-  } else {
-    console.log("prev");
+  } else if (current_slide == 2) {
+    const country = $("#country-field");
+    const address = $("#address-field");
+    const education = $("#education-field");
+    const skills = $("#skills-field");
+
+    if (country.val() == "" || address.val() == "") {
+      alert.text(
+        "Please fill out all required information before proceeding..."
+      );
+      alert.fadeIn().addClass("show");
+      setTimeout(() => {
+        alert.fadeOut().removeClass("show");
+      }, 1500);
+    } else {
+      slide.classList.remove("d-block");
+      slide.classList.add("d-none");
+      nextSlide.classList.remove("d-none");
+      nextSlide.classList.add("d-block");
+      sign_submit.removeAttribute("disabled");
+      current_slide = 3;
+    }
   }
 });
 
@@ -101,6 +119,8 @@ $(".prev-fields").click((ev) => {
   slide.classList.add("d-none");
   prevSlide.classList.remove("d-none");
   prevSlide.classList.add("d-block");
+  sign_submit.setAttribute("disabled", "disabled");
+  current_slide -= 1;
 });
 
 $(".select-pic").click(() => {
